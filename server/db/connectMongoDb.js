600134a0ca991bdf1449bb6700d4be65e5919d85
Capalot <baruchefiok@gmail.com>
2025-04-14 06:58:02 +0100

    Add fallback MongoDB connection URI for local development

--- server/db/connectMongoDb.js
+++ server/db/connectMongoDb.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectMongoDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.mongo_uri);
+    const conn = await mongoose.connect(
+      process.env.mongo_uri || "mongodb://127.0.0.1:27017/x-clone"
+    );
     console.log(`Mongo connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
